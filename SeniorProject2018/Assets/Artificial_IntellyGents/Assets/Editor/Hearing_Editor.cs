01d9ef9103b43c3db89d1300bd42d8835d2c06c3
Nathan Lively <ntlively@ufl.edu>
2018-01-26 17:01:32 -0500

    FOV+FOS CHANGES
    For Simplicity:
    FOS is now Hearing
    FOV is now Vision
    Implementation Changes:
    Hearing now decays based off distance and obstacles, but still needs work.  Incomplete.
    Need to make sphere colliders radius change based off produced db levels.

---
+++ SeniorProject2018/Assets/Artificial_IntellyGents/Assets/Editor/Hearing_Editor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (Hearing))]
+public class FOS_Editor : Editor {
+	void OnSceneGUI(){
+		EnableFOS();
+	}
+
+	//Field of Sound Additional Details On Character Selection
+	void EnableFOS(){
+		// Changes the editor to draw a field of sound visualization on the selected target
+		Hearing fos = (Hearing)target;
+		// Set color of sound visualization
+		Handles.color = Color.white;
+		// Draw the max range circle of the field of sound for the target
+		Handles.DrawWireArc(fos.transform.position,Vector3.up,Vector3.forward,360,fos.soundRadius);
+		// Draw sound radius lines
+		Vector3 soundAngleA = fos.DirFromAngle (-fos.soundAngle / 2, false);
+		Vector3 soundAngleB = fos.DirFromAngle (fos.soundAngle/2,false);
+		Handles.DrawLine (fos.transform.position,fos.transform.position + soundAngleA*fos.soundRadius);
+		Handles.DrawLine (fos.transform.position,fos.transform.position + soundAngleB*fos.soundRadius);
+		// Draw line between selected and hearableTarget
+		Handles.color = Color.red;
+		foreach (Transform hearableTarget in fos.hearableTargets) {
+			Handles.DrawLine (fos.transform.position, hearableTarget.position);
+		}
+	}
+
+}
