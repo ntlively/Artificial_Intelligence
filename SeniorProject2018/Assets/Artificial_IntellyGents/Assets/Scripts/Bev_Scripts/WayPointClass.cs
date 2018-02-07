5cdd67e6830b3a78cfeeac47d84c5924024c81ab
Beverly Roberts <beverlyr380@ufl.edu>
2018-02-07 15:04:53 -0500

    Combining hearing and vision to bev AI script

---
+++ SeniorProject2018/Assets/Artificial_IntellyGents/Assets/Scripts/Bev_Scripts/WayPointClass.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class WayPointClass : MonoBehaviour {
+
+	[SerializeField]
+	protected float debugDrawRadius = 0.5F;
+	
+	//Visited 
+	public bool visited;
+	public bool preyEvidence;
+	public float evidenceTimer;
+
+	//List<WayPointClass> waypoints;
+
+	void Start ()
+	{
+		//Set the waypoint to unvisited
+		visited = false;
+
+		//Set timer 
+		preyEvidence = false;
+		evidenceTimer = 20.0f;
+	}
+
+	//Updates everytime
+	void Update ()
+	{
+		if(preyEvidence)
+		{
+			evidenceTimer -= Time.deltaTime;
+			if (evidenceTimer <= 0 )
+			{
+				this.resetPreyEvidence(false);
+				this.resetPreyTime();
+			}
+		}
+	}
+
+	//Displays waypoints
+	public virtual void OnDrawGizmos () 
+	{
+		if(this.tag == "Waypoint")
+		{
+			//Visited by the predator
+			if(visited == true)
+			{
+				Gizmos.color = Color.red;
+			}
+			else 
+			{
+				Gizmos.color = Color.yellow;
+			}
+		}
+		else if (this.tag == "Finish")
+		{
+			Gizmos.color = Color.blue;
+		}
+
+		Gizmos.DrawWireSphere(transform.position,debugDrawRadius);
+
+		if(preyEvidence)
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(transform.position,1.0f);
+		}
+	}
+
+// -----------------------------------------------------------------------------------------
+	//Gets and sets
+//------------------------------------------------------------------------------------------
+	public bool getVisit(){ return visited; }
+
+	public bool getPreyEvidence(){ return preyEvidence; }
+
+	public void resetVisit()
+	{
+		if(visited)
+		{	
+			visited = false;
+		}
+		else
+		{
+			visited = true;
+		}
+	}
+
+	public void resetPreyEvidence(bool cur){ preyEvidence = cur;}
+
+	public void resetPreyTime()
+	{
+		//Reset the timer 
+		evidenceTimer = 20.0f;
+	}
+
+
+
+
+}
---
+++ SeniorProject2018/Assets/Artificial_IntellyGents/Assets/Scripts/Nate_Scripts/predDirector.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// BUG REPORT
+// 1) ON TRIGGER ENTER DOES NOT TRIGGER IF PLAYER SPAWNS INSIDE SIGHT RADIUS
+// 2) NO CASE FOR LEAVING SPHERE COLLIDER TRIGGER ZONE
+// 3) CHASE SPEED IN SCRIPT NOT SETTING PROPERLY
+// 4) DON'T NEED SPHERE COLLIDER, BUT WHERE TO PUT VISION CHECK BESIDES ON TRIGGER ENTER
+
+
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+	public class predDirector : MonoBehaviour {
+
+		// Variable Declarations
+		public UnityEngine.AI.NavMeshAgent agent;
+		public ThirdPersonCharacter character;
+
+		public enum State{
+			PATROL,
+			CHASE
+		}
+
+		public State state;
+		private bool alive;
+
+		// Variables for PATROL
+		public GameObject[] waypoints;
+		private int waypointINDEX = 0;
+		public float patrolSpeed = 0.5f;
+
+
+		// Variables for CHASE
+		public float chaseSpeed = 0.1f;
+		public GameObject target;
+
+
+		// Use this for initialization
+		void Start () {
+			agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+			character = GetComponent<ThirdPersonCharacter>();
+
+			agent.updatePosition = true;
+			agent.updateRotation = false;
+
+			state = predDirector.State.PATROL;
+			alive = true;
+
+
+			//start finite state machine (FSM)
+			StartCoroutine("FSM");
+			
+		}
+
+		IEnumerator FSM()
+		{
+			while(alive)
+			{
+				switch(state)
+				{
+					case State.PATROL:
+						Patrol();
+						break;
+					case State.CHASE:
+						Chase();
+						break;
+				}
+				yield return null;
+			}
+		}
+		
+		void Patrol()
+		{
+			agent.speed = patrolSpeed;
+			if(Vector3.Distance(this.transform.position,waypoints[waypointINDEX].transform.position)>= 2)
+			{
+				agent.SetDestination(waypoints[waypointINDEX].transform.position);
+				character.Move(agent.desiredVelocity,false,false); //velocity, crouch, jump
+			}
+			else if (Vector3.Distance(this.transform.position,waypoints[waypointINDEX].transform.position)<=2)
+			{
+				waypointINDEX += 1;
+				if(waypointINDEX >= waypoints.Length)
+				{
+					waypointINDEX = 0;
+				}
+			}
+			else
+			{
+				character.Move(Vector3.zero,false,false);
+			}
+		}
+
+		void Chase()
+		{
+			agent.speed = chaseSpeed;
+			agent.SetDestination(target.transform.position);
+			character.Move(agent.desiredVelocity,false,false);
+		}
+
+		void OnTriggerEnter (Collider coll)
+		{
+			Vision script = GameObject.Find("AIThirdPersonController").GetComponent<Vision>();
+			bool viewCheck = false;
+
+			foreach (Transform visibleTarget in script.visibleTargets) {
+
+				if(visibleTarget.CompareTag("Player")){
+					print("TRUE");
+					viewCheck = true;
+				}
+			}
+			//print(coll.tag+":"+viewCheck);
+			//coll.tag == "Player"  &&
+
+			if(viewCheck)
+			{
+				state = predDirector.State.CHASE;
+				target = coll.gameObject;
+			}
+		}
+		// Update is called once per frame
+		// void Update () {
+			
+		// }
+	}	
+}
