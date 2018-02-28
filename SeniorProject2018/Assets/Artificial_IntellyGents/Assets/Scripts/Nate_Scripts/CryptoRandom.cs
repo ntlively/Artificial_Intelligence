59a510e91f4d559ddff3bbdd23cc8f0e76fbedb6
Nathan Lively <ntlively@ufl.edu>
2018-02-28 16:04:09 -0500

    Added Neural Net Code
    Not used or tested yet.  
    Source: https://code.msdn.microsoft.com/Basis-of-Neural-Networks-f12ce1e9

---
+++ SeniorProject2018/Assets/Artificial_IntellyGents/Assets/Scripts/Nate_Scripts/CryptoRandom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using UnityEngine;
+namespace NeuralNet.NeuralNet
+{
+    public class CryptoRandom
+    {
+        public double RandomValue { get; set; }
+
+        public CryptoRandom()
+        {
+                try
+                {
+                    RNGCryptoServiceProvider p = new RNGCryptoServiceProvider();
+                    System.Random r = new System.Random(p.GetHashCode());
+                    this.RandomValue = r.NextDouble();
+                    
+                }
+                finally
+                {
+                    Debug.Log("RNGCryptoServiceProvider in CryptoRandom.cs has failed.  Most likely because it is not System.IDisposable");
+                }
+        }
+
+    }
+}
